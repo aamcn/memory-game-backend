59142a5c1a0dcc7fb99a3c6a6037e5570b290ffc
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-14 18:22:42 +0100

    Chore: Update easyLeaderBoardQueries to match database naming

--- db/easyLeaderBoardQueries.js
+++ db/easyLeaderBoardQueries.js
@@ -1,18 +1,18 @@
 const pool = require("./pool");
 
 async function getAllEasyScores() {
-  return await pool.query("SELECT * FROM four_card_leader_board;");
+  return await pool.query("SELECT * FROM easy_leader_board;");
 }
 
 async function getEasyScoreById(id) {
   return await pool.query(
-    `SELECT * FROM four_card_leader_board WHERE id = ${id};`,
+    `SELECT * FROM easy_leader_board WHERE id = ${id};`,
   );
 }
 
 async function addEasyHighScorer(id, name, time, score) {
   await pool.query(
-    "INSERT INTO four_card_leader_board (id, name, time, score) VALUES ($1, $2, $3, $4)",
+    "INSERT INTO easy_leader_board (id, name, time, score) VALUES ($1, $2, $3, $4)",
     [id, name, time, score],
   );
 }
