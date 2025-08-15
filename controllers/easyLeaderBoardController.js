c8f51f53d9097f9e6409a66106bf308999bc1d16
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-15 12:17:53 +0100

    Refactor: easyLeaderBoardController to handle errors and tidy code

--- controllers/easyLeaderBoardController.js
+++ controllers/easyLeaderBoardController.js
@@ -1,26 +1,38 @@
 const query = require("../db/easyLeaderBoardQueries");
-
 async function getEasyTest(req, res) {
-  
-  res.send('Success');
+  res.status(200).send('Success');
 }
 
-async function getAllEasyScores(req, res) {
+async function getAllEasyScores(req, res, next) {
   const allScores = await query.getAllEasyScores();
-  res.send(allScores.rows);
+  try {
+  res.status(200).send(allScores.rows);
+  } catch (error) {
+    next(error); // Forward error to global handler
+  }
 }
 
-async function getEasyScoreById(req, res) {
-  const id = req.body.id;
-  const score = await query.getEasyScoreById(id);
-  res.send(score.rows);
+async function getEasyScoreById(req, res, next) {
+    try {
+      const id = req.body.id;
+      const score = await query.getEasyScoreById(id);
+      res.status(200).send(score.rows);
+    } catch (error) {
+      next(error);
+  }
 }
 
-async function addEasyHighScorer(req, res) {
-  const { playerName, finishTime } = req.body;
-  await query.addEasyHighScorer(playerName, finishTime);
-  res.send(200);
+async function addEasyHighScorer(req, res, next) {
+  try {
+    const { playerName, finishTime } = req.body;
+    await query.addEasyHighScorer(playerName, finishTime);
+    res.status(200).send('Success');
+  } catch (error) {
+    next(error);
+  }
 }
+
+
 
 module.exports = {
   getEasyTest,
